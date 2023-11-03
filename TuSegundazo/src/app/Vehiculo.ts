ef23f217d3723c92c6726cd92f25785d4090eea9
Alejandro Ramirez <ramirezalejo@live.com>
2023-11-03 15:52:27 -0400

    creacion del componente y el model de vehiculo

---
+++ TuSegundazo/src/app/Vehiculo.ts
@@ -0,0 +1,23 @@
+export class Vehiculo {
+
+    constructor(id:string, marca:string, linea:string, referencia:string, modelo:number, kilometraje:number, color:string, imagen:string) {
+        this.id = parseInt(id);
+        this.marca = marca;
+        this.linea = linea;
+        this.referencia = referencia;
+        this.modelo = modelo;
+        this.kilometraje = kilometraje;
+        this.color = color;
+        this.imagen = imagen;
+    
+    }
+    
+    id: number;
+    marca: string;
+    linea: string;
+    referencia: string;
+    modelo: number;
+    kilometraje: number;
+    color: string;
+    imagen: string;
+}
--- TuSegundazo/src/app/app.component.ts
+++ TuSegundazo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { VehiculoModule } from './vehiculo/vehiculo.module';
 
 @Component({
   selector: 'app-root',
