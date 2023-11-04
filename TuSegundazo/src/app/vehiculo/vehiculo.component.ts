af720a7da59289806ee1f8659960250330488039
Alejandro Ramirez <ramirezalejo@live.com>
2023-11-03 22:00:07 -0400

    Modificar el componente para incluir totales

--- TuSegundazo/src/app/vehiculo/vehiculo.component.ts
+++ TuSegundazo/src/app/vehiculo/vehiculo.component.ts
@@ -20,4 +20,12 @@ export class VehiculoComponent implements OnInit {
     this.vehiculosService.getVehiculos().subscribe(vehiculos => this.vehiculos = vehiculos);
   }
 
+  get marcas() {
+    return this.vehiculos.map(vehiculo => vehiculo.marca).filter((marca, index, array) => array.indexOf(marca) === index);
+  }
+
+  carrosPorMarca(marca: string) {
+    return this.vehiculos.filter(vehiculo => vehiculo.marca === marca).length;
+  }
+
 }
